e7cee1c20aba0a39620916589292de9be967924b
konuk <konuk@301-001>
2024-04-26 21:03:27 +0300

    Web Api created

---
+++ E-Trade.DAL/Abstract/ICategoryDAL.cs
@@ -0,0 +1,14 @@
+﻿using E_Trade.Business.Abstract;
+using E_Trade.Data.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Trade.DAL.Abstract
+{
+    public interface ICategoryDAL : IGenericRepository<Category>
+    {
+    }
+}
